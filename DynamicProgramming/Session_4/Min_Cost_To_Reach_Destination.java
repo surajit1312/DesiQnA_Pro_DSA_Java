37e866608de25d638711d14b3569796fa446e45b
surajitrana <surajit1602@gmail.com>
2024-08-25 01:11:29 +0530

    feat[dp][session-4][amazon-oa]: minimum cost to reach destination

---
+++ DynamicProgramming/Session_4/Min_Cost_To_Reach_Destination.java
@@ -0,0 +1,43 @@
+package DynamicProgramming.Session_4;
+
+public class Min_Cost_To_Reach_Destination {
+    public static void main(String[] args) {
+        int n = 6;
+        int[] costs = { 4, 12, 13, 18, 10, 12 };
+        int minCost = getMinimumCost(costs, n);
+        System.out.println(minCost);
+    }
+
+    /**
+     * Optimal Approach
+     * 
+     * TC: O(N)
+     * SC: O(N)
+     * 
+     * @param costs
+     * @param n
+     * @return
+     */
+    private static int getMinimumCost(int[] costs, int n) {
+        int[] dp = new int[n]; // SC: O(N)
+        dp[0] = 0;
+        dp[1] = Math.abs(costs[1] - costs[0]);
+        dp[2] = Math.abs(costs[2] - costs[1]) + dp[1];
+        /**
+         * Manual calculation
+         * 
+         * dp[3] = Math.min(x, y) where x and y are:
+         * int x = Math.abs(costs[3] - costs[2]) + dp[2]
+         * in x, best cost to reach at 2 is added
+         * int y = Math.abs(costs[3] - costs[0]) + dp[0]
+         * in y, best cost to reach at 0 is added
+         * as ith traveller can travel from i to (i + 1)th or (i + 3)th airport
+         */
+        for (int i = 3; i < n; i++) { // TC: O(N)
+            int x = Math.abs(costs[i] - costs[i - 1]) + dp[i - 1];
+            int y = Math.abs(costs[i] - costs[i - 3]) + dp[i - 3];
+            dp[i] = Math.min(x, y);
+        }
+        return dp[n - 1];
+    }
+}
---
+++ DynamicProgramming/Session_4/README.md
@@ -0,0 +1,28 @@
+## Problem: Minimum cost to reach the destination | Amazon ML Summer School | Sep 2023 |
+
+### Input 1:
+
+    int n = 6
+    int[] costs = { 4, 12, 13, 18, 10, 12 }
+
+### Output:
+    10
+
+### Input 2:
+
+    int n = 4
+    int[] costs = { 1, 4, 5, 2 }
+
+### Output:
+    1
+
+Link-> https://www.desiqna.in/15983/amazon-summer-school-sep-2023-minimum-cost-reach-destination 
+
+
+
+PseudoCode.-> https://ideone.com/cCStp1 
+
+
+C++ https://ideone.com/eoCHX7 
+Java. https://ideone.com/Szh6bR 
+Py https://ideone.com/xkhWiS. 
