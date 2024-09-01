72fb4d6cc16c4462806552baecebe3265ca06a00
surajitrana <surajit1602@gmail.com>
2024-09-02 01:13:32 +0530

    feat[binary-search][session-3][leetcode-33]: search in a rotated sorted array

---
+++ BinarySearch/Session_3/Search_In_Rotated_Sorted_Array.java
@@ -0,0 +1,37 @@
+package BinarySearch.Session_3;
+
+public class Search_In_Rotated_Sorted_Array {
+    public static void main(String[] args) {
+        int[] nums = { 4, 5, 6, 7, 0, 1, 2 };
+        int target = 0;
+        int targetIndex = search(nums, target);
+        System.out.println(targetIndex);
+    }
+
+    public static int search(int[] nums, int target) {
+        int n = nums.length;
+        int low = 0;
+        int high = n - 1;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] >= nums[low]) {
+                // left part is sorted
+                if (target >= nums[low] && target < nums[mid]) {
+                    high = mid - 1;
+                } else {
+                    low = mid + 1;
+                }
+            } else {
+                // right part is sorted
+                if (target > nums[mid] && target <= nums[high]) {
+                    low = mid + 1;
+                } else {
+                    high = mid - 1;
+                }
+            }
+        }
+        return -1;
+    }
+}
